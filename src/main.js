9a888000b39b6209fa73b1fd44f6c8a98abf2ba9
yuriwillian <yuriwilian@gmail.com>
2022-05-31 17:58:31 -0300

    portifolio

---
+++ src/main.js
@@ -0,0 +1,28 @@
+
+function abrirMenu() {
+    menuOculto = document.getElementById("menuOculto");
+    menuOculto.style.display = "block"
+}
+
+
+function esconderMenu() {
+    menuOculto = document.getElementById("menuOculto");
+    menuOculto.style.display = "none"
+}
+
+
+
+/**
+ * 
+ * 
+ * function mostraSub(sub){
+  subfilho = document.getElementById(sub);
+  subfilho.style.display = 'block';
+}
+
+function escondeSub(sub){
+  subfilho = document.getElementById(sub);
+  subfilho.style.display = 'none'; 
+}
+ * 
+ */
